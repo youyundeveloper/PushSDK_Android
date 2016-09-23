c4a7b8bd61f31b576bb6f46479fd26127d990829
ywb <weibiao@17shihui.com>
2016-09-23 15:53:11 +0800

    游云和小米push统一，添加点击事件

--- sample/YouyunPushDome/app/src/main/java/cn/youyunpushdome/PushMsgReceiver.java
+++ sample/YouyunPushDome/app/src/main/java/cn/youyunpushdome/PushMsgReceiver.java
@@ -1,72 +1,34 @@
 package cn.youyunpushdome;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.widget.RemoteViews;
 
-import com.weimi.push.WeimiPushReceiver;
+import com.weimi.push.ClickReceiver;
 import com.weimi.push.data.PayLoadMessage;
 
-import yun.mi.push.test.R;
-
 /**
- * Created by 卫彪 on 2016/5/11.
+ * Created by 卫彪 on 2016/9/23.
  */
-public class PushMsgReceiver extends WeimiPushReceiver {
-	private int rq = 0;
-	private Notification notification;
+public class PushMsgReceiver extends ClickReceiver {
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void onMessage(Context context, PayLoadMessage payLoadMessage) {
-		String alert = payLoadMessage.alert;
-		Log.v("Bill", "alert:" + alert);
-		if (alert == null || alert.equals(""))
-			return;
-		Intent intent1 = new Intent();
-		intent1.setClass(context, MainActivity.class);
-		intent1.putExtra("pushMsg", alert);
-		PendingIntent pendingIntent = PendingIntent.getActivity(context, rq++,
-				intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+    /**
+     * 集成游云PUSH的接收消息会回调
+     *
+     * @param context
+     * @param payLoadMessage 消息数据
+     */
+    @Override
+    protected void onYouYunReceiveMessage(Context context, PayLoadMessage payLoadMessage) {
+        super.onYouYunReceiveMessage(context, payLoadMessage);
+    }
 
-		NotificationManager nManager = (NotificationManager) context
-				.getSystemService(Context.NOTIFICATION_SERVICE);
-
-		if (notification == null) {
-			notification = new Notification(R.mipmap.ic_launcher,
-					context.getResources().getString(R.string.app_name), System.currentTimeMillis());
-			notification.contentView = new RemoteViews(
-					context.getPackageName(), R.layout.notifycation_layout);
-		}
-		notification.contentView.setTextViewText(R.id.tv1, alert);
-		notification.contentView.setViewVisibility(R.id.tv1, View.VISIBLE);
-		notification.contentView.setViewVisibility(R.id.tv2, View.GONE);
-
-		notification.icon = R.mipmap.ic_launcher;
-		notification.tickerText = alert;
-		notification.flags = Notification.FLAG_AUTO_CANCEL | Notification.FLAG_SHOW_LIGHTS;
-		notification.contentIntent = pendingIntent;
-		notification.ledARGB = 0xff00ff00;
-		notification.ledOnMS = 500;
-		notification.ledOffMS = 2000;
-		nManager.cancel(300);
-		nManager.notify(300, notification);
-
-		PushUtil.context = context;
-		if (payLoadMessage.sound != null) {
-			if(SharedPreferenceUtil.getInstance().getVibration()){
-				SoundVibrateUtil.checkIntervalTimeAndVibrate(context);
-			}
-			if(SharedPreferenceUtil.getInstance().getSound()){
-				SoundVibrateUtil.checkIntervalTimeAndSound(context);
-			}
-		}
-
-		// TODO Auto-generated method stub
-	}
+    /**
+     * 点击回调
+     *
+     * @param context
+     * @param message 消息数据
+     */
+    @Override
+    protected void onNotificationClicked(Context context, Object message) {
+        super.onNotificationClicked(context, message);
+    }
 }
