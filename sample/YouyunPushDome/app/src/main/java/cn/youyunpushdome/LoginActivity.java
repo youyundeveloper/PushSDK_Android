df2e746a0bad89f59cb458aeac9800309aa33aa5
ywb <weibiao@17shihui.com>
2016-12-26 21:29:48 +0800

    华为推送修改

--- sample/YouyunPushDome/app/src/main/java/cn/youyunpushdome/LoginActivity.java
+++ sample/YouyunPushDome/app/src/main/java/cn/youyunpushdome/LoginActivity.java
@@ -1,9 +1,11 @@
 package cn.youyunpushdome;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -25,6 +27,34 @@ import yun.mi.push.test.R;
  * modify on 2016/11/8
  */
 public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * 集成游云PUSH需要四步即可,调用请参考下面initSDK()方法
+     * 1、调用 YouyunInstance.getInstance().initSDK() 初始化SDK 成功返回 {"apistatus":1,"result":"xxxxx"}，result为用户id
+     * 2、第1步成功回调中调用YouyunInstance.getInstance().startPush()启动服务
+     * 3、调用YouyunInstance.getInstance().registerEquipment()注册设备信息
+     * 4、在AndroidManifest中的广播中配置appid
+     *   <receiver
+     *      android:name="com.weimi.push.WeimiPushReceiver"
+     *      android:exported="false">
+     *          <intent-filter>
+     *              <action android:name="me.weimi.push.action.20142" />
+     *              <action android:name="me.weimi.push.action.20525" />
+     *          </intent-filter>
+     *      </receiver>
+     *
+     *
+     *  通过以上四步就可以收到push推送了，在任何android手机都可以接收push了
+     *  上面功能需要两个push推送基础包youyun-push-android-1.1.1.jar和youyun-protobuf-java-2.4.1.jar和http网络包volley.jar
+     *
+     *  如果想在小米手机上使用小米push则需要加入MiPush_SDK_Client_3_0_3.jar
+     *  如果想在华为手机上使用华为push则需要加入HwPush_SDK_V2705.jar
+     *  否则所有手机都使用游云推送功能
+     *
+     */
+
+
+
 
     private boolean isOnline = false;
 
@@ -50,6 +80,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
     }
 
     private void initSDK() {
@@ -123,4 +154,16 @@ public class LoginActivity extends AppCompatActivity {
         }
         return OpenUDID;
     }
+
+    /**
+     * 获取手机IMEI号
+     *
+     * 在华为开放平台推送华为需要用imei推送
+     */
+    public static String getIMEI(Context context) {
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+        String imei = telephonyManager.getDeviceId();
+        return imei;
+    }
+
 }
